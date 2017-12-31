bf6a826e34f05c5d0aaf8a0f46d2dba2178741bc
FarhadF <farhad.farahi@gmail.com>
2017-12-31 18:57:08 +0330

    todo

--- api-gateway/controllers/movies.go
+++ api-gateway/controllers/movies.go
@@ -6,7 +6,7 @@ import (
 	"net/url"
 	"net/http/httputil"
 )
-
+//TODO: Find a way to handle http: proxy error: dial tcp 192.168.163.196:8082: getsockopt: connection refused when backend is not available
 func ReverseMovies(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	target := &url.URL{Scheme: "http", Host: "192.168.163.196:8081"}
 	proxy := httputil.NewSingleHostReverseProxy(target)
