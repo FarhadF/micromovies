0d871afa357608af30e296855d2bc05ec4aa0fe8
FarhadF <farhad.farahi@gmail.com>
2018-01-10 18:46:08 +0330

    added movies protected endpoint and login endpoint on api gateway

--- api-gateway/controllers/movies.go
+++ api-gateway/controllers/movies.go
@@ -5,6 +5,9 @@ import (
 	"github.com/julienschmidt/httprouter"
 	"net/url"
 	"net/http/httputil"
+	"github.com/golang/glog"
+	"encoding/json"
+	"micromovies/api-gateway/token"
 )
 //TODO: Find a way to handle http: proxy error: dial tcp 192.168.163.196:8082: getsockopt: connection refused when backend is not available
 func ReverseMovies(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
@@ -12,8 +15,40 @@ func ReverseMovies(w http.ResponseWriter, r *http.Request, _ httprouter.Params) 
 	proxy := httputil.NewSingleHostReverseProxy(target)
 	proxy.ServeHTTP(w,r)
 }
+
 func ReverseMovie(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	target := &url.URL{Scheme: "http", Host: "192.168.163.196:8081"}
 	proxy := httputil.NewSingleHostReverseProxy(target)
 	proxy.ServeHTTP(w,r)
+}
+
+func ReverseMovieProtected(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	tokenString, err := token.ExtractToken(r)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusInternalServerError)
+		resp := json.RawMessage(`{"error":"` + err.Error() + `"}`)
+		w.Write(resp)
+		glog.Error(err)
+	} else {
+
+		tokenStatus := token.ValidateToken(tokenString, "user")
+		glog.Info(tokenStatus)
+		if tokenStatus == true {
+			target := &url.URL{Scheme: "http", Host: "192.168.163.196:8082"}
+			proxy := httputil.NewSingleHostReverseProxy(target)
+			proxy.ServeHTTP(w, r)
+		} else {
+			w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+			w.WriteHeader(http.StatusForbidden)
+			resp := json.RawMessage(`{"status":"forbidden"}`)
+			w.Write(resp)
+		}
+	}
+
+	/*} else {
+	target := &url.URL{Scheme: "http", Host: "192.168.163.196:8082"}
+	proxy := httputil.NewSingleHostReverseProxy(target)
+	proxy.ServeHTTP(w, r)
+}*/
 }
--- api-gateway/router/router.go
+++ api-gateway/router/router.go
@@ -10,14 +10,15 @@ func New() *httprouter.Router {
 	router.GET("/movies", controllers.ReverseMovies)
     router.POST("/movie", controllers.ReverseMovie)
     router.GET("/movie/*get", controllers.ReverseMovie)
-    router.DELETE("/movie/*delete", controllers.ReverseMovie)
-    router.POST("/movie/*post", controllers.ReverseMovie)
+    router.DELETE("/movie/*delete", controllers.ReverseMovieProtected)
+    router.POST("/movie/*post", controllers.ReverseMovieProtected)
 	router.GET("/users", controllers.ReverseUsers)
 	router.POST("/user", controllers.ReverseUser)
 	router.GET("/user/id/*get", controllers.ReverseUserProtected)
 	router.GET("/user/email/*get", controllers.ReverseUser)
 	router.DELETE("/user/*delete", controllers.ReverseUser)
 	router.POST("/user/*post", controllers.ReverseUser)
+	router.POST("/login", controllers.ReverseUser)
 
 	return router
 }
